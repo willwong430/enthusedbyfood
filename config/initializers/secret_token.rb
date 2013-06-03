e724b25541e0ab37a8d1c912721ca67475df65c9
William Wong <willwong430@gmail.com>
2013-06-03 16:32:43 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Agreedupon1::Application.config.secret_key_base = 'b49e7bd1bcf4a6fb75fde343399bb9067fa944a2f9a59b8e34c5f71b162cc5da7bf42cf459d1c7de3b843b8e7667a13957ff4c7a36aabace730b365490384f51'
