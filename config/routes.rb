74540c7c651a7431ebe0de5ce1e15c9618084b17
William Wong <willwong430@gmail.com>
2013-06-12 13:58:50 -0400

    Finish sign in

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,10 @@
 Agreedupon1::Application.routes.draw do
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
   root to: 'static_pages#home'
   match '/signup',    to: 'users#new',            via: 'get'
+  match '/signin',    to: 'sessions#new',         via: 'get'
+  match '/signout',   to: 'sessions#destroy',     via: 'delete'
   match '/about',     to: 'static_pages#about',   via: 'get'
   match '/start',     to: 'static_pages#start',   via: 'get'
   match '/mission',   to: 'static_pages#mission', via: 'get'
