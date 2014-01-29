80f0e6bcdb4ba01506f69185954d42d9a0dbd37e
William Wong <willwong430@gmail.com>
2014-01-29 18:10:11 -0500

    Fixed JavaScript

--- app/assets/javascripts/form.js
+++ app/assets/javascripts/form.js
@@ -4,20 +4,16 @@ $(function() {
 		if (attached == "photo") {
 			$("#attach_photo").show();
 			$("#attach_link").hide();
-			$("#attach_story").hide();
 		}
 		else if (attached == "link") {
 			$("#attach_link").show();
 			$("#attach_photo").hide();
-			$("#attach_story").hide();
 		}
 		else {
-			$("#attach_story").hide();
 			$("#attach_link").hide();
 			$("#attach_photo").hide();
 		}
 	});
-
 })();
 
 
