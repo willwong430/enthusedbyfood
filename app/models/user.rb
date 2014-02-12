87323f34ce4df36f5600316c723ea694e5b771ed
William Wong <willwong430@gmail.com>
2014-02-12 15:43:32 -0500

    Fixed admin method

--- app/models/user.rb
+++ app/models/user.rb
@@ -16,7 +16,7 @@ class User < ActiveRecord::Base
   has_many :followed_cuisines, through: :relationships, source: :cuisine
   before_save { self.email = email.downcase }
   before_create :create_remember_token
-  after_update :set_admin
+  after_create :set_admin
   validates :name,  presence: true, length: { maximum: 50 }
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
   validates :email, presence:   true,
