fa4209fdb112cff25dcd2cacba4b5bcdf2afb6b7
willwong430 <willwong430@gmail.com>
2018-09-26 19:59:26 -0400

    aws create signature

--- app/controllers/signed_urls_controller.rb
+++ app/controllers/signed_urls_controller.rb
@@ -19,7 +19,7 @@ class SignedUrlsController < ApplicationController
           ["starts-with", "$key", "uploads/"],
           { acl: 'public-read' },
           { success_action_status: '201' },
-          {:'x-amz-credential' => "AKIAIN4ZPUQJIFDBZ7OA/20151229/us-east-1/s3/aws4_request"},
+          {:'x-amz-credential' => "AKIAIN4ZPUQJIFDBZ7OA/20180925/us-east-1/s3/aws4_request"},
           {:'x-amz-algorithm' => "AWS4-HMAC-SHA256"},
           {:'x-amz-date' => "20180925T000000Z" }
         ]
@@ -27,11 +27,20 @@ class SignedUrlsController < ApplicationController
     ).gsub(/\n|\r/, '')
   end
 
+  def getSignatureKey key, dateStamp, regionName, serviceName
+    kDate = OpenSSL::HMAC.digest('sha256', "AWS4" + key, dateStamp)
+    kRegion = OpenSSL::HMAC.digest('sha256', kDate, regionName)
+    kService = OpenSSL::HMAC.digest('sha256', kRegion, serviceName)
+    kSigning = OpenSSL::HMAC.digest('sha256', kService, "aws4_request")
+    kSigning
+  end
+
+
   def s3_upload_signature
     Base64.encode64(
       OpenSSL::HMAC.digest(
         OpenSSL::Digest::Digest.new('sha1'),
-        ENV['AWS_SECRET_ACCESS_KEY'],
+        getSignatureKey(ENV['AWS_SECRET_ACCESS_KEY'], '20180925', 'us-east-1', 's3'),
         s3_upload_policy_document
       )
     ).gsub(/\n/, '')
